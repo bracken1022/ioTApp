77e6482ffd8eec8a167ffc3a4544fa1b80518198
bracken1983 <bracken@1921682102.local>
2016-01-02 23:42:23 +0800

    play sound when open the door

--- detectDistance.py
+++ detectDistance.py
@@ -1,5 +1,7 @@
+
 #!/usr/bin/python env
 
+import os
 import time
 import RPi.GPIO as GPIO
 
@@ -15,24 +17,24 @@ def initGpioPin(inPin, outPin, pinType):
     GPIO.setup(outPin, GPIO.OUT)
     GPIO.setup(outPin, GPIO.OUT, initial=GPIO.LOW)
 
-def detectDis(triggerPin, echoPin, pinType):
+def detectDis(triggerPin, echoPin):
 
-    initGpioPin(echoPin, triggerPin, pinType)
+    initGpioPin(echoPin, triggerPin, GPIO.BCM)
 
     #wait 2 seconds to start get distance
-    time.sleep(2)
+    time.sleep(0.5)
     GPIO.output(triggerPin, GPIO.HIGH)
-    time.sleep(0.000015)
+    time.sleep(0.000015) 
     GPIO.output(triggerPin, GPIO.LOW)
 
     while receiveLowVoltage(echoPin):
-        pass
+        pass    
     startTime = time.time()
 
     while receiveHighVoltage(echoPin):
         pass
     endTime = time.time()
-
+    
     interval = endTime - startTime
     distance = interval * 340 / 2
 
@@ -41,10 +43,27 @@ def detectDis(triggerPin, echoPin, pinType):
 def gpioCleanup():
     GPIO.cleanup()
 
+
+DOOR_OPEN = 1
+DOOR_CLOSED = 2
+currentDoorStatus = DOOR_CLOSED
+doorStatus = DOOR_CLOSED
+
 try:
     while True:
-        distance = detectDis(2, 3, GPIO.BCM)
-        print "distance is %f" % distance
+        distance = detectDis(2, 3)
+        if distance < 0.95:
+            if doorStatus == DOOR_OPEN:
+                pass
+            else:
+                doorStatus = DOOR_OPEN
+                print "door is open %f" %distance
+                os.system("mplayer /home/pi/python_program/comeOnBaby.m4a < /dev/null > /dev/null 2>1&")
+        else:
+            if doorStatus == DOOR_CLOSED:
+                pass
+            else:
+                doorStatus = DOOR_CLOSED
+                print "door is closed %f" %distance
 except KeyboardInterrupt:
     gpioCleanup()
-
